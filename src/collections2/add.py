4a81c1638b44d753427e93795de15ae9739b10e8
Lucas Roberts <rlucas@Lucass-MacBook.local>
2024-10-18 17:04:55 -0400

    add module docstring

--- src/collections2/add.py
+++ src/collections2/add.py
@@ -1,4 +1,7 @@
 # simple function to test build
+""" Here I'm just adding a module level docstring to check in a change to the
+starter repo and test things out...
+"""
 
 def add(x, y):
     """Compute the sum of two numbers."""
